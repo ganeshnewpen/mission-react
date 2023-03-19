725452548f8f0f59df30678d33b77548face23ca
augusta <ganeshneupane968@gmail.com>
2023-03-19 13:43:57 +0545

    learned JSX and function absed components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         I am Augusta
-        </a>
-      </header>
-    </div>
+    <>
+    <div className="lets-start">Hey</div>
+    </>
   );
 }
 
